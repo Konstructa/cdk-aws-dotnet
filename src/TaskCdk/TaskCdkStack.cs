dfc151dde9bc91bf9da490550ffd2c23ba3fc917
Milena Elen <elenprojects7@gmail.com>
2023-02-15 09:41:58 -0300

    FEAT: vpc, security group, eks, bastion and database

--- src/TaskCdk/TaskCdkStack.cs
+++ src/TaskCdk/TaskCdkStack.cs
@@ -1,7 +1,10 @@
 using Amazon.CDK;
 using Amazon.CDK.AWS.AutoScaling;
 using Amazon.CDK.AWS.EC2;
+using Amazon.CDK.AWS.ElasticLoadBalancingV2;
+using Amazon.CDK.AWS.EKS;
 using Constructs;
+using Amazon.CDK.AWS.RDS;
 
 namespace TaskCdk
 {
@@ -10,34 +13,96 @@ namespace TaskCdk
         internal TaskCdkStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
         {
             Vpc vpc = new Vpc(this, "VPC", new VpcProps {
-                IpAddresses = IpAddresses.Cidr("10.0.1.0/20"),
+                IpAddresses = IpAddresses.Cidr("10.0.1.0/16"),
                 MaxAzs = 3,
-                //SubnetConfiguration = new[] { new SubnetConfiguration {
-                //}, new SubnetConfiguration {
-                //    CidrMask = 24,
-                //    Name = "Application",
-                //    SubnetType = SubnetType.PUBLIC
-                //}, new SubnetConfiguration {
-                //    CidrMask = 28,
-                //    Name = "Database",
-                //    SubnetType = SubnetType.PRIVATE_ISOLATED,
-
-                //    // 'reserved' can be used to reserve IP address space. No resources will
-                //    // be created for this subnet, but the IP range will be kept available for
-                //    // future creation of this subnet, or even for future subdivision.
-                //    Reserved = true
-                //}}
+                SubnetConfiguration = new[] { new SubnetConfiguration 
+                {
+                    }, new SubnetConfiguration {
+                        CidrMask = 24,
+                        Name = "Ingress",
+                        SubnetType = SubnetType.PUBLIC,
+                    },new SubnetConfiguration {
+                        CidrMask = 24,
+                        Name = "Application",
+                        SubnetType = SubnetType.PRIVATE_WITH_EGRESS,
+                    }, new SubnetConfiguration {
+                        CidrMask = 28,
+                        Name = "Database",
+                        SubnetType = SubnetType.PRIVATE_ISOLATED,
+                    }
+                }
             });
 
-            SecurityGroup mySecurityGroup = new SecurityGroup(this, "SecurityGroup", new SecurityGroupProps { Vpc = vpc });
+            SecurityGroup mySecurityGroup = new SecurityGroup(this, "SecurityGroup", new SecurityGroupProps 
+            {
+                Vpc = vpc, 
+                SecurityGroupName = "SecurityGroup",
+                AllowAllOutbound= true
+            });
 
 
             AutoScalingGroup autoScalingGroup =  new AutoScalingGroup(this, "ASG", new AutoScalingGroupProps
             {
                 Vpc = vpc,
-                InstanceType = InstanceType.Of(InstanceClass.BURSTABLE2, InstanceSize.MICRO),
+                InstanceType = InstanceType.Of(InstanceClass.T2, InstanceSize.MICRO),
                 MachineImage = new AmazonLinuxImage(),
                 SecurityGroup = mySecurityGroup
+            });
+
+            // aplication load balancer, EKS, Bastion e Banco (RDS ou Aurora)
+
+            ApplicationLoadBalancer lb = new ApplicationLoadBalancer(this, "LB", new ApplicationLoadBalancerProps 
+            {
+                Vpc = vpc,
+                SecurityGroup = mySecurityGroup,
+                InternetFacing = true,
+                VpcSubnets = new SubnetSelection { SubnetType = SubnetType.PUBLIC }
+            });
+
+            ApplicationListener applicationListener = lb.AddListener("Listener", new BaseApplicationListenerProps
+            {
+                Port = 80,
+                Open = true
+            });
+
+            applicationListener.AddTargets("ApplicationFleet", new AddApplicationTargetsProps
+            {
+                Port = 8080,
+                Targets = new [] { autoScalingGroup }
+            });
+
+            Cluster cluster = new Cluster(this, "EKS", new ClusterProps { 
+               Version = KubernetesVersion.V1_21,
+            });
+
+            cluster.AddAutoScalingGroupCapacity("frontend-nodes", new AutoScalingGroupCapacityOptions
+            {
+                InstanceType = new InstanceType("t2.micro"),
+                MinCapacity = 2,
+                VpcSubnets = new SubnetSelection { SubnetType = SubnetType.PUBLIC }
+            });
+
+
+            DatabaseCluster databaseCluster = new DatabaseCluster(this, "Database", new DatabaseClusterProps
+            {
+                Engine = DatabaseClusterEngine.AuroraMysql(new AuroraMysqlClusterEngineProps
+                {
+                    Version = AuroraMysqlEngineVersion.VER_2_09_2
+                }),
+
+                InstanceProps = new Amazon.CDK.AWS.RDS.InstanceProps
+                {
+                    InstanceType = new InstanceType("t2.micro"),
+                    VpcSubnets = new SubnetSelection { SubnetType = SubnetType.PRIVATE_WITH_EGRESS, SubnetGroupName = "Database" },
+                    Vpc = vpc
+                },
+                
+            });
+
+
+            BastionHostLinux bastionHostLinux = new BastionHostLinux(this, "BastionHost", new BastionHostLinuxProps 
+            { 
+                Vpc = vpc,
             });
 
 
