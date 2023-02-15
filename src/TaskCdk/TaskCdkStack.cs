4137a6ebdd37676e585da5db2e6354200a967a78
Milena Elen <elenprojects7@gmail.com>
2023-02-15 14:37:23 -0300

    Refactor

--- src/TaskCdk/TaskCdkStack.cs
+++ src/TaskCdk/TaskCdkStack.cs
@@ -13,6 +13,7 @@ namespace TaskCdk
         internal TaskCdkStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
         {
             Vpc vpc = new Vpc(this, "VPC", new VpcProps {
+                IpAddresses = IpAddresses.Cidr("10.0.0.0/16"),
                 MaxAzs = 2,
                 SubnetConfiguration = new SubnetConfiguration[] {
                 new SubnetConfiguration
@@ -26,18 +27,49 @@ namespace TaskCdk
                     Name = "Database",
                     CidrMask = 28,
                     SubnetType = SubnetType.PRIVATE_WITH_EGRESS,
+                },
+                new SubnetConfiguration
+                {
+                    Name = "Bastion",
+                    CidrMask = 24,
+                    SubnetType = SubnetType.PUBLIC,
                 }
             }
+
             });
 
+            //sim eu sei das questões de segurança,
+            //pode permitir que os recursos dentro da VPC sejam acessados de forma não autorizada pela Internet
+            ((Subnet)vpc.PrivateSubnets[0]).AddRoute("AppRoute", new AddRouteOptions
+            {
+                RouterId = vpc.InternetGatewayId,
+                RouterType = RouterType.GATEWAY,
+                DestinationCidrBlock = "0.0.0.0/0"
+            });
 
-            SecurityGroup SecurityGroup = new SecurityGroup(this, "SecurityGroup", new SecurityGroupProps 
+            ((Subnet)vpc.PrivateSubnets[1]).AddRoute("DatabaseRoute", new AddRouteOptions
+            {
+                RouterId = vpc.InternetGatewayId,
+                RouterType = RouterType.GATEWAY,
+                DestinationCidrBlock = "0.0.0.0/0"
+            });
+
+            ((Subnet)vpc.PublicSubnets[0]).AddRoute("BastionRoute", new AddRouteOptions
+            {
+                RouterId = vpc.InternetGatewayId,
+                RouterType = RouterType.GATEWAY,
+                DestinationCidrBlock = "0.0.0.0/0"
+            });
+  
+
+            SecurityGroup SecurityGroup = new SecurityGroup(this, "MySecurityGroup", new SecurityGroupProps 
             {
                 Vpc = vpc, 
                 SecurityGroupName = "SecurityGroup",
                 AllowAllOutbound= true
             });
 
+            //restringir o acesso a essas portas somente aos IPs necessários ou a um grupo específico de IPs confiáveis.
             SecurityGroup.AddIngressRule(Peer.AnyIpv4(), Port.Tcp(80), "Allow HTTP access");
             SecurityGroup.AddIngressRule(Peer.AnyIpv4(), Port.Tcp(443), "Allow HTTPS access");
 
@@ -75,13 +107,12 @@ namespace TaskCdk
                Version = KubernetesVersion.V1_21,
             });
 
-            cluster.AddAutoScalingGroupCapacity("frontend-nodes", new AutoScalingGroupCapacityOptions
+            cluster.AddAutoScalingGroupCapacity("app-nodes", new AutoScalingGroupCapacityOptions
             {
                 InstanceType = new InstanceType("t2.micro"),
                 MinCapacity = 2,
                 VpcSubnets = new SubnetSelection { SubnetType = SubnetType.PRIVATE_WITH_EGRESS }
             });
-
 
             new DatabaseCluster(this, "Database", new DatabaseClusterProps
             {
@@ -112,7 +143,7 @@ namespace TaskCdk
             new BastionHostLinux(this, "BastionHost", new BastionHostLinuxProps 
             { 
                 Vpc = vpc,
-                SubnetSelection = new SubnetSelection { SubnetType = SubnetType.PRIVATE_WITH_EGRESS },
+                SubnetSelection = new SubnetSelection { SubnetType = SubnetType.PUBLIC },
                 InstanceType = new InstanceType("t2.micro"),
                 MachineImage = new AmazonLinuxImage(),
                 SecurityGroup = bastionSecurityGroup,
